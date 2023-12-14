c6e781e4c214ad1b10bcdc5e207bfc6c548c894e
WebLock <antontamazlukar@gmail.com>
2023-12-15 00:25:29 +0100

    Add prettier, pre-commit, husky
    Change several eslint config options.

--- src/main.js
+++ src/main.js
@@ -1,71 +1,55 @@
-import "./style/style.css";
+import './style/style.css'
 
 async function prepareAdapter() {
   if (!navigator.gpu) {
-    return console.error("This browser don't support WebGPU.");
+    return console.error("This browser don't support WebGPU.")
   }
 
-  const adapter = await navigator.gpu.requestAdapter();
+  const adapter = await navigator.gpu.requestAdapter()
 
   if (!adapter) {
-    return console.error("No appropriate GPUAdapter found.");
+    return console.error('No appropriate GPUAdapter found.')
   }
 
-  return adapter;
+  return adapter
 }
 
 async function init() {
-  const adapter = await prepareAdapter();
+  const adapter = await prepareAdapter()
 
-  const device = await adapter.requestDevice();
-  const encoder = device.createCommandEncoder();
-  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-  const canvas = document.getElementById("board");
-  const context = canvas.getContext("webgpu");
+  const device = await adapter.requestDevice()
+  const encoder = device.createCommandEncoder()
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat()
+  const canvas = document.getElementById('board')
+  const context = canvas.getContext('webgpu')
 
-  context.configure({
-    device: device,
-    format: canvasFormat,
-  });
+  context.configure({ device: device, format: canvasFormat })
 
   const vertices = new Float32Array([
-    //   X,    Y,
-    -0.8,
-    -0.8, // Triangle 1 (Blue)
-    0.8,
-    -0.8,
-    0.8,
-    0.8,
-
-    -0.8,
-    -0.8, // Triangle 2 (Red)
-    0.8,
-    0.8,
-    -0.8,
-    0.8,
-  ]);
+    -0.8, -0.8, 0.8, -0.8, 0.8, 0.8, -0.8, -0.8, 0.8, 0.8, -0.8, 0.8,
+  ])
 
   const vertexBuffer = device.createBuffer({
-    label: "Cell vertices",
+    label: 'Cell vertices',
     size: vertices.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-  });
+  })
 
-  device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/ 0, vertices);
+  device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/ 0, vertices)
 
   const vertexBufferLayout = {
     arrayStride: 8,
     attributes: [
       {
-        format: "float32x2",
+        format: 'float32x2',
         offset: 0,
         shaderLocation: 0, // Position, see vertex shader
       },
     ],
-  };
+  }
 
   const cellShaderModule = device.createShaderModule({
-    label: "Cell shader",
+    label: 'Cell shader',
     code: `
       // Your shader code will go here
       @vertex
@@ -78,47 +62,47 @@ async function init() {
         return vec4f(0.79, 0.65, 0.96, 1); // (R, G, B, A)
       }
     `,
-  });
+  })
 
   const cellPipeline = device.createRenderPipeline({
-    label: "Cell pipeline",
-    layout: "auto",
+    label: 'Cell pipeline',
+    layout: 'auto',
     vertex: {
       module: cellShaderModule,
-      entryPoint: "vertexMain",
+      entryPoint: 'vertexMain',
       buffers: [vertexBufferLayout],
     },
     fragment: {
       module: cellShaderModule,
-      entryPoint: "fragmentMain",
+      entryPoint: 'fragmentMain',
       targets: [
         {
           format: canvasFormat,
         },
       ],
     },
-  });
+  })
 
   const pass = encoder.beginRenderPass({
     colorAttachments: [
       {
         view: context.getCurrentTexture().createView(),
-        loadOp: "clear",
+        loadOp: 'clear',
         clearValue: { r: 0.11, g: 0.11, b: 0.18, a: 1 },
-        storeOp: "store",
+        storeOp: 'store',
       },
     ],
-  });
+  })
 
-  pass.setPipeline(cellPipeline);
-  pass.setVertexBuffer(0, vertexBuffer);
-  pass.draw(vertices.length / 2); // 6 vertices
+  pass.setPipeline(cellPipeline)
+  pass.setVertexBuffer(0, vertexBuffer)
+  pass.draw(vertices.length / 2) // 6 vertices
 
-  pass.end();
-  const commandBuffer = encoder.finish();
-  device.queue.submit([commandBuffer]);
+  pass.end()
+  const commandBuffer = encoder.finish()
+  device.queue.submit([commandBuffer])
 
-  console.log(canvas);
+  console.log(canvas)
 }
 
-init();
+init()
